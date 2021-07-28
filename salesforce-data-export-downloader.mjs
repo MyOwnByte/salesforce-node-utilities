96202e3bf516d90aa8c3ffc2d31f2114e6a1aed3
myownbyte <myownbyte@gmail.com>
2021-07-28 08:18:54 -0300

    Cleanup code

--- salesforce-data-export-downloader.mjs
+++ salesforce-data-export-downloader.mjs
@@ -3,10 +3,7 @@ import downloadData from './src/handlers/salesforce-data-export-downloader.mjs';
 
 async function init() {
     dotenv.config();
-    console.log('process.env.Username:::', process.env.SalesforceUsername);
-    console.log('process.env.Password:::', process.env.SalesforcePassword);
-    console.log('process.env.SecurityToken:::', process.env.SalesforceSecurityToken);
-    console.log('process.env.SalesforceDataExportFileDownloader_BackupFolder:::', process.env.SalesforceDataExportFileDownloader_BackupFolder);
+
     downloadData([null, null,
         process.env.SalesforceUrl,
         process.env.SalesforceUsername, 
--- src/handlers/salesforce-data-export-downloader.mjs
+++ src/handlers/salesforce-data-export-downloader.mjs
@@ -2,20 +2,26 @@ import SalesforceConnection from '../SalesforceConnection.mjs';
 import SalesforceDataExportFileDownloader from '../SalesforceDataExportFileDownloader.mjs';
 
 async function downloadData(args) {
-    // console.log('args:::', args);
+    const url = args[2];
+    const username = args[3];
+    const password = args[4];
+    const securityToken = args[5];
+    const backupFolder = args.length == 7 ? args[6] : './backups/';
+
     if(args.length < 6 || args.length > 7) {
         console.log(`Usage: 
             salesforce-data-export-downloader SalesforceUrl SalesforceUsername SalesforcePassword SalesforceSecurityToken [BackupFolderPath]`);
         return 0;
     }
-    let backupFolder = args.length == 7 ? args[6] : './backups/';
-    let sfConnection = new SalesforceConnection(args[2],args[3],args[4],args[5]);
+    
+    let sfConnection = new SalesforceConnection(url, username, password, securityToken);
     await sfConnection.connect();
 
     let downloader = new SalesforceDataExportFileDownloader(sfConnection.connection.instanceUrl, 
         sfConnection.connection.accessToken, 
         backupFolder);
     downloader.download();
+    downloader.asdf();
 }
 
 export default downloadData;
